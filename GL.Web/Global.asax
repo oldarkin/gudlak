2b793cd83be770bd65bf05b1d677c57d8d201df9
oldarkin <oldarkin@rambler.ru>
2016-04-17 20:15:38 +0400

    [Add project]

---
+++ GL.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GL.Web.MvcApplication" Language="C#" %>
---
+++ GL.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GL.Web.Startup))]
+namespace GL.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
